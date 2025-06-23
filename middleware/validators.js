763fa509adef0a23371416665e77a6894eb872ad
Ayesha Ahmed <138239898+bonjour-paris@users.noreply.github.com>
2025-06-23 14:51:25 +0400

    Update validators.js

--- middleware/validators.js
+++ middleware/validators.js
@@ -1,8 +1,9 @@
 const { body } = require('express-validator');
+
 exports.validateRegister = [
   body('name').notEmpty().withMessage('Name is required'),
   body('email').isEmail().withMessage('Invalid email format'),
-  body('password').isLength({ min: 6 }).withMessage('Password must be 6+ characters')
+  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
 ];
 
 exports.validateProduct = [
