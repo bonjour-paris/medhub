de6f027a50450df08a3a0cdd3d1cab87a4080164
Ayesha Ahmed <138239898+bonjour-paris@users.noreply.github.com>
2025-06-30 11:37:13 +0400

    Update index.js

--- to-do backend/index.js
+++ to-do backend/index.js
@@ -1,26 +1,11 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config(); 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
-const taskRoutes = require('./routes/tasks');
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-// Middlewares
-app.use(cors());
-app.use(express.json());
-
-// Routes
-app.use('/api/tasks', taskRoutes);
-
-// Connect to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/todoapp')
-  .then(() => {
-    console.log(' MongoDB connected!!');
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
-  })
-  .catch((err) => console.error(' MongoDB connection error:', err));
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
